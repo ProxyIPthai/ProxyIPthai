5f5e463859eec0c56e22f50501fb73c25764a969
unknown <thiwwichai69@gmail.com>
2023-10-26 00:07:15 +0700

    update

--- src/components/Price.vue
+++ src/components/Price.vue
@@ -1,5 +1,6 @@
 <template>
-        <div class="grid grid-cols-6 gap-6 w-full max-w-5xl ">
+        <div class="grid grid-cols-6 gap-6 w-full max-w-5xl backdrop-blur-md bg-black/20 p-10 rounded-md">
+            <p class="col-span-6 text-[38px] text-[#38B6FF] drop-shadow-md">ราคาแพ็คโปรโมชั่น</p>
             <div class="col-span-2 bg-white py-5 rounded-md drop-shadow-md flex-col flex justify-start p-10">
                 <p class="text-left text-[32px] font-bold drop-shadow-md text-[#38B6FF]">ราคาเริ่มต้น</p>
                 <ul class="list-disc pl-5 text-[18px] font-semibold drop-shadow-md mt-10 text-[#7c97a8]">
